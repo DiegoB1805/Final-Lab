3f3144f7341fa68b3a2fdce09bfcd4115bb4ec50
Diego Bello <98779180+DiegoB1805@users.noreply.github.com>
2025-04-08 16:13:20 -0400

    cambio de variable(nombre)

--- main.py
+++ main.py
@@ -6,34 +6,34 @@ from checkers.game import Game
 
 FPS = 60
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
-def get_row_col_from_mouse(pos):
-    x, y = pos
-    row = y // SQUARE_SIZE
-    col = x // SQUARE_SIZE
+def get_row_col_from_mouse(position):
+    x, y = position
+    row = y // SQUARE_SIZE # Calcula fila basada en posición Y
+    col = x // SQUARE_SIZE # Calcula columna basada en posición X
     return row, col
 
 def main():
     run = True
     clock = pygame.time.Clock()
-    game = Game(WIN)
+    game = Game(WINDOW) # Crea una instancia del juego 
 
     while run:
         clock.tick(FPS)
-
+          # Verifica si hay un ganador
         if game.winner() != None:
             print(game.winner())
             run = False
-
+          # Manejo de eventos
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                row, col = get_row_col_from_mouse(pos)
+                position = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(position)
                 game.select(row, col)
 
         game.update()
